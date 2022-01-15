6f17655ee1c7f5dfcfee92cabfd4843147c833d3
malav2001 <95087683+malav2001@users.noreply.github.com>
2022-01-15 15:29:20 +0530

    Update Script-2.py

--- Python Scripts/Script-2.py
+++ Python Scripts/Script-2.py
@@ -1,18 +1,12 @@
-# from googletrans import Translator
+from youtube_transcript_api import YouTubeTranscriptApi
+import os
 
-# translator = Translator()
-# result = translator.translate('Mikä on nimesi', src='fi', dest='fr')
+srt = YouTubeTranscriptApi.get_transcript("dhYOPzcsbGM")
+text = ""
 
-# print(result.src)
-# print(result.dest)
-# print(result.text)
+with open("file.txt", "w") as file:
+    for i in srt:
+        text += i['text'] + '\n'
+    file.write(text)
 
-from googletrans import Translator
-
-with open('spanish_text.txt') as f:
-    text_to_translate = f.read()
-    translator = Translator()
-    # print(translator.detect(text_to_translate))
-    result = translator.translate(text_to_translate,dest='en').text
-with open('translated_spanish_text_into_english.txt','w') as f:
-    f.write(result)
+os.startfile("file.txt")
